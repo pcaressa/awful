936555e85c968d49e949fead5cecc881078797df
pcaressa <paolo@caressa.it>
2023-10-28 00:46:43 +0200

    wip: awful.c

--- c/awful.c
+++ c/awful.c
@@ -1,4 +1,4 @@
-/** awful.c */
+/** \file awful.c */
 
 #include <assert.h>
 #include <ctype.h>
@@ -44,13 +44,19 @@ typedef union {
     double n;           // Number
     char *t;            // string
     struct stack_s *s;  // stack or closure
+    void *p;            // generic pointer: used for functions
 } val_t;
 
+/** Stack item. */
 typedef struct stack_s {
-    enum {NUMBER = -5, STRING, ATOM, STACK, CLOSURE} type;
+    enum {NUMBER = -6, STRING, ATOM, KEYWORD,
+        STACK, CLOSURE} type;
     val_t val;
     struct stack_s *next;
 } *stack_t;
+
+/** Free stack. */
+static stack_t stack_freelist = NULL;
 
 /** Print a value on a file. */
 void val_printf(FILE *file, int type, val_t v)
@@ -91,12 +97,31 @@ void val_printf(FILE *file, int type, val_t v)
 /** Push a value (t,v) on the stack s. */
 stack_t stack_push(stack_t s, int t, val_t v)
 {
-    stack_t tos = malloc(sizeof(struct stack_s));
-    assert(tos != NULL);
+    stack_t tos;
+    if (stack_freelist == NULL) {
+        tos = malloc(sizeof(struct stack_s));
+        assert(tos != NULL);
+    } else {
+        tos = stack_freelist;
+        stack_freelist = stack_freelist->next;
+    }
     tos->type = t;
     tos->val = v;
     tos->next = s;
     return tos;
+}
+
+/** Discard the top of a stack passed by reference;
+    the new stack pointer is written into s but also
+    returned as value. */
+stack_t stack_drop(stack_t *s)
+{
+    assert(s != NULL);
+    stack_t tos = *s;
+    *s = (*s)->next;
+    tos->next = stack_freelist;
+    stack_freelist = tos;
+    return *s;
 }
 
 /** Reverse the order of elements in a stack. */
@@ -105,9 +130,11 @@ stack_t stack_reverse(stack_t s)
     if (s == NULL || s->next == NULL) {
         return s;
     }
+    /* for each triple of consecutive elements a->b->c
+        inverts the links as a<-b<-c. */
     stack_t prev = NULL;
     stack_t here = s;
-    stack_t next = here->next;
+    stack_t next = s->next;
 
     while (next->next != NULL) {
         here->next = prev;
@@ -140,18 +167,24 @@ void stack_delete(stack_t s)
             stack_delete(body);
             stack_delete(env);
         }
-        stack_t next = s->next;
-        free(s);
-        s = next;
+        stack_drop(&s);
+        /*stack_t next = s->next;
+        // free(s);
+        s->next = stack_freelist;
+        stack_freelist = s;
+        s = next;*/
     }
 }
 
+/* *****  Lexical analyzer  ***** */
+
 /** Scan a list of tokens from text, using any character
-    in delimiters as delimiter.
+    in delimiters as delimiter; the stack keywords is a
+    stack of consecutive pairs name->value->... where
+    values are void * pointers.
     A stack is returned, containing the list of tokens
-    in reverse order, so that the last one is its top.
-*/
-stack_t scan(char *text, char *delimiters)
+    in reverse order, so that the last one is its top. */
+stack_t scan(char *text, char *delimiters, stack_t keywords)
 {
     assert(text != NULL);
     stack_t tokens = NULL;
@@ -168,7 +201,7 @@ stack_t scan(char *text, char *delimiters)
             char q = *text;
             char *p = strchr(text + 1, q);
             except_on(p == NULL, "End of text inside string");
-            v.t = str_new(text + 1, p - text - 2);
+            v.t = str_new(text + 1, p - text - 1);
             tokens = stack_push(tokens, STRING, v);
             text = p + 1;
         } else {
@@ -178,25 +211,166 @@ stack_t scan(char *text, char *delimiters)
             && strchr(delimiters, *text) == NULL)
                 ++ text;
             v.t = str_new(p, text - p);
-            tokens = stack_push(tokens, ATOM, v);
+
+            // Check against a keyword.
+            for (stack_t p = keywords; p != NULL; p = p->next->next) {
+                // p is a stack with two elements: name->pointer
+                assert(p->type == ATOM);
+                if (strcmp(v.t, p->val.t) == 0) {
+                    v.p = p->next->val.p;
+                    tokens = stack_push(tokens, KEYWORD, v);
+                    break;
+                }
+            }
+            if (p == NULL) {
+                // Check against a number.
+                char *q;
+                double n = strtod(p, &q);
+                if (q == text) {
+                    v.n = n;
+                    tokens = stack_push(tokens, NUMBER, v);
+                } else {
+                    tokens = stack_push(tokens, ATOM, v);
+                }
+            }
         }
     }
     return tokens;
 }
 
-stack_t awful(char *text)
+/* *****  Interpreter  ***** */
+
+// Referenced before being defined:
+stack_t awful_eval(stack_t *r_tokens, stack_t env);
+
+/*
+    Keywords routines.
+*/
+
+#define GETXY() \
+    stack_t y = awful_eval(tokens, env);    \
+    except_on(y->type != NUMBER, "Number expected");    \
+    stack_t x = awful_eval(tokens, env);    \
+    except_on(x->type != NUMBER, "Number expected");
+
+stack_t ADD(stack_t *tokens, stack_t env)
 {
-    stack_t tokens = NULL;
-    if (setjmp(except_buf) == 0) {
-        tokens = scan(text, "(){},:");
-        tokens = stack_reverse(tokens);
-        for (stack_t s = tokens; s != NULL; s = s->next) {
-            val_printf(stdout, s->type, s->val);
-            putchar(' ');
+    GETXY();
+    stack_t retval = stack_push(NULL, NUMBER, x->val);
+    retval->val.n += y->val.n;
+    return retval;
+}
+
+stack_t SUB(stack_t *tokens, stack_t env)
+{
+    GETXY();
+    stack_t retval = stack_push(NULL, NUMBER, x->val);
+    retval->val.n -= y->val.n;
+    return retval;
+}
+
+typedef stack_t (*keyword_t)(stack_t*, stack_t);
+
+static struct { char *s; keyword_t k;} KEYWORDS[] = {
+    {"ADD", ADD},
+    {"SUB", SUB},
+    {NULL, NULL}
+};
+
+/** Looks for an atom inside an stack of environments:
+    if found then the stack item containing it is
+    returned, else NULL. */
+stack_t awful_find(char *t, stack_t e)
+{
+    for (; e != NULL; e = e->next) {
+        /* We expect e to be a stack of pairs
+            name->value -> name->value -> ... */
+        assert(e->type == STACK);
+        for (stack_t p = e->val.s; p != NULL; p = p->next->next) {
+            assert(p->type == STRING);
+            if (strcmp(t, p->val.t) == 0) {
+                return p->next;
+            }
         }
     }
+    return NULL;
+}
+
+/** \note
+
+    The interpreter expects in input a stack whose
+    elements are "tokens", thus each item is a value
+    representing a value or a symbol.
+*/
+
+/** Interpret a token list, w.r.t. an environment, both
+    passed by reference, and return the stack with the
+    result of the evaluation (tos = value of the expression).
+    On error, return NULL. */
+stack_t awful_eval(stack_t *r_tokens, stack_t env)
+{
+    stack_t tokens = *r_tokens;
+    stack_t retval;
+    except_on(tokens == NULL, "Unexpected end of text");
+    switch (tokens->type) {
+    case NUMBER: case STRING:
+        retval = tokens;
+        *r_tokens = tokens->next;
+        break;
+    case ATOM:
+        retval = awful_find(tokens->val.t, env);
+        except_on(retval == NULL, "Undefined variable %s", tokens->val.t);
+        break;
+    case KEYWORD:
+        // A keyword has the address of its routine as value
+        retval = ((keyword_t)(tokens->val.p))(stack_drop(r_tokens), env);
+        break;
+    case '{':
+        assert(!"TODO");
+    case '(':
+        assert(!"TODO");
+    default:
+        except_on(1, "Bad expression");
+    }
+    return retval;
+}
+
+/** Interpret a string and return the corresponding
+    value as a stack with a single item: if an error
+    occurs, NULL is returned. */
+stack_t awful(char *text)
+{
+    if (setjmp(except_buf) != 0) {
+        return NULL;
+    }
+    // Creates the stack containing the consecutive
+    // pairs name->address->... for keywords.
+    stack_t keywords = NULL;
+    for (int i = 0; KEYWORDS[i].s != NULL; ++ i) {
+        val_t v;
+        v.p = KEYWORDS[i].k;
+        keywords = stack_push(keywords, KEYWORD, v);
+        v.t = KEYWORDS[i].s;
+        keywords = stack_push(keywords, ATOM, v);
+    }
+
+    stack_t tokens = scan(text, "(){},:", keywords);
+    tokens = stack_reverse(tokens);
+
+    for (stack_t s = tokens; s != NULL; s = s->next) {
+        val_printf(stdout, s->type, s->val);
+        putchar(' ');
+    }
+
+    // Creates the default environment: a stack containing
+    // a stack of consecutive pairs name->address of keywords.
+    val_t v; v.s = keywords;
+    stack_t env = stack_push(env, STACK, v);
+    stack_t retval = awful_eval(&tokens, &env);
+    stack_delete(env);
     stack_delete(tokens);
-    return tokens;
+
+    return retval;
 }
 
 int main(int argc, char **argv)
